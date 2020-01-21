2532fafe99d73b368231ab53dcb7b65334a46ae8
Rebecca <tayloreb@oregonstate.edu>
2020-01-21 12:14:28 -0600

    Fix PID expansion

--- smallsh/smallsh.c
+++ smallsh/smallsh.c
@@ -157,41 +157,46 @@ char* promptCommandLine()
 /******************************************************************************
  * Takes an input string and its character count
  * Expands $$ to PID before returning string
+ * Resource: https://stackoverflow.com/questions/9052490/find-the-count-of-substring-in-string
+ * Resource: https://stackoverflow.com/questions/779875/what-is-the-function-to-replace-string-in-c
 *******************************************************************************/
 char *expandPID(char *inputString)
 {
     char* pid = calloc(32, sizeof(char));
-    char* temp = calloc(MAX_CHARS, sizeof(char));
+    char* insertPoint;
+    char* temp;
+    char* result;
+    int count = 0;
+    int lenFront = 0;       // Length from front when finding index for expansion
 
     //Save current PID
     sprintf(pid, "%d", getpid());
 
-    //Copy values from original string to temp
-    int j = 0;                                              //Index for temp string
-    for (int i = 0; i < numChars-1; i++) {
-
-        temp[j] = inputString[i];
-
-        //If find $$
-        if (inputString[i] == '$' && inputString[i+1] == '$') {
-
-            temp[i] = '\0';                                 //Remove first '$'
-            strcat(temp, pid);                              //Add pid to temp
-
-            j += (int)strlen(pid) - 1;                      //Move j to account for PID length
-            i++;                                            //Skip second '$'
-        }
-
-        j++;
+    //Find all instances of '$$'
+    temp = inputString;
+    while ((temp = strstr(temp, EXPAND_PID))) {
+        temp += strlen(EXPAND_PID);
+        count++;
     }
 
-    //Copy temp to original input string, free memory, and return
-    sprintf(inputString, "%s", temp);
+    //Allocate space for all expansions
+    temp = result = calloc(strlen(inputString) + (strlen(pid) - strlen(EXPAND_PID)) * count + 1,
+                           sizeof(char));
+
+    //Return original string without modifications if nothing to expand
+    if (count == 0) { strcpy(result, inputString); }
+
+    //Else replace all instances of '$$' with PID
+    while (count--) {
+        insertPoint = strstr(inputString, EXPAND_PID);          // Find next '$$'
+        lenFront = insertPoint - inputString;                   // Calc distance from front
+        temp = strncpy(temp, inputString, lenFront) + lenFront; // Copy non-PID part of string
+        temp = strcpy(temp, pid) + strlen(pid);                 // Replace '$$' with PID
+        inputString += lenFront + strlen(EXPAND_PID);           // Move to next instance
+    }
 
     free(pid);
-    free(temp);
-
-    return inputString;
+    return result;
 }
 
 /******************************************************************************
--- smallsh/testPIDexpansion.c
+++ smallsh/testPIDexpansion.c
@@ -8,11 +8,26 @@
 #include <string.h>
 #include "smallsh.h"
 
+char* testPIDExpansion(char* inputString)
+{
+    char* result;
+
+    printf("\ninput string: %s\n", inputString);
+    result = expandPID(inputString);
+
+    return result;
+}
+
+//Resource: https://stackoverflow.com/questions/9052490/find-the-count-of-substring-in-string
+//Resource: https://stackoverflow.com/questions/779875/what-is-the-function-to-replace-string-in-c
 char *expandPID(char *inputString)
 {
     char* pid = calloc(32, sizeof(char));
+    char* insertPoint;
     char* temp;
+    char* result;
     int count = 0;
+    int lenFront = 0;       // Length from front when finding index for expansion
 
     //Save current PID
     sprintf(pid, "%d", getpid());
@@ -23,63 +38,57 @@ char *expandPID(char *inputString)
         temp += strlen(EXPAND_PID);
         count++;
     }
-    printf("Count: %d\n", count);
+
+    //Allocate space for all expansions
+    temp = result = calloc(strlen(inputString) + (strlen(pid) - strlen(EXPAND_PID)) * count + 1,
+                           sizeof(char));
 
     //Return original string without modifications if nothing to expand
-    if (count == 0) { free(pid); return inputString; }
+    if (count == 0) { strcpy(result, inputString); }
 
     //Else replace all instances of '$$' with PID
     while (count--) {
-        printf("\tcount: %d\n", count);
+        insertPoint = strstr(inputString, EXPAND_PID);          // Find next '$$'
+        lenFront = insertPoint - inputString;                   // Calc distance from front
+        temp = strncpy(temp, inputString, lenFront) + lenFront; // Copy non-PID part of string
+        temp = strcpy(temp, pid) + strlen(pid);                 // Replace '$$' with PID
+        inputString += lenFront + strlen(EXPAND_PID);           // Move to next instance
     }
 
     free(pid);
-    return inputString;
+    return result;
 }
 
 int main()
 {
     printf("\nTest PID expansion\n");
-
-    char* userInput = calloc(32, sizeof(char));
+    char* testResult;
 
     // Test with no expansion
-    sprintf(userInput, "userInput");
-    printf("user input: %s\n", userInput);
-
-    userInput = expandPID(userInput);
-    printf("Expanded user input: %s\n\n", userInput);
+    testResult = testPIDExpansion("user input");
+    printf("Expanded result: %s\n", testResult);
 
     // Test with one expansion
-    sprintf(userInput, "testPID $$");
-    printf("user input: %s\n", userInput);
-
-    userInput = expandPID(userInput);
-    printf("Expanded user input: %s\n\n", userInput);
+    testResult = testPIDExpansion("testPID $$");
+    printf("Expanded result: %s\n", testResult);
 
     // Test with two consecutive expansions
-    sprintf(userInput, "testPID $$ $$");
-    printf("user input: %s\n", userInput);
-
-    userInput = expandPID(userInput);
-    printf("Expanded user input: %s\n\n", userInput);
+    testResult = testPIDExpansion("testPID $$ $$");
+    printf("Expanded result: %s\n", testResult);
 
     // Test with two consecutive expansions no spaces
-    sprintf(userInput, "testPID$$$$");
-    printf("user input: %s\n", userInput);
-
-    userInput = expandPID(userInput);
-    printf("Expanded user input: %s\n\n", userInput);
+    testResult = testPIDExpansion("testPID$$$$");
+    printf("Expanded result: %s\n", testResult);
 
     // Test with two non-consecutive expansions
-    sprintf(userInput, "testPID$$testPID$$");
-    printf("user input: %s\n", userInput);
+    testResult = testPIDExpansion("testPID$$testPID$$");
+    printf("Expanded result: %s\n", testResult);
 
-    userInput = expandPID(userInput);
-    printf("Expanded user input: %s\n\n", userInput);
+    // Test with only '$$' (x4)
+    testResult = testPIDExpansion("$$$$$$$$");
+    printf("Expanded result: %s\n", testResult);
 
-    free(userInput);
-
+    free(testResult);
     return 0;
 }
 
